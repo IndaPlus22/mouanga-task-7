291e385d0ca1ef1d9092c924842acfc45331c518
salticecream <113557800+salticecream@users.noreply.github.com>
2022-11-04 09:32:22 +0100

    Update aDp.c

--- c_andy/aDp.c
+++ c_andy/aDp.c
@@ -4,7 +4,7 @@
 int main() {
     long long a, b = 0;
 
-    while ( scanf("%lld %lld", &a, &b) == 2) {
+    while ( scanf("%lld %lld", &a, &b) == 2) { // för while-satsen tog jag hjälp av kattis tutorial-lösning för a different problem
         printf("%lld\n", llabs(a - b));
     }
 
